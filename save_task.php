5b5cab5e50f479fd5dfec09eac9824bd5b995ef3
JesusWiedii <jesus.becerra@wiedii.co>
2019-11-12 17:01:51 -0500

    final no da mas

--- save_task.php
+++ save_task.php
@@ -8,16 +8,20 @@ if(isset($_POST['guardar_persona'])){
     $huella=$_POST['huellanuevo'];
     
     if(!empty($nombre) && !empty($correo) && !empty($huella)){
-        $repetido= mysqli_query($conn,"");
-        if (empty($repetido)){
-            $query ="INSERT INTO usuarios(nombre_usu, correo, equipo, huella) 
-            VALUES('$nombre', '$correo', '$equipo','$huella')";
-            $result= mysqli_query($conn, $query);
-            $mensaje="9";
+        $repetido= "SELECT * FROM usuarios WHERE huella = $huella";
+        $res_huella=mysqli_query($conn,$repetido);
+        if (mysqli_num_rows($res_huella)>1){
+            echo 'puedes editar';
         }
-        elseif(!empty($repetido)){
+        
+        //if (empty($res_huella)){
+        //    $query ="INSERT INTO usuarios(nombre_usu, correo, equipo, huella) 
+        //    VALUES('$nombre', '$correo', '$equipo','$huella')";
+         //   $result= mysqli_query($conn, $query);}
+       
+         elseif(!empty($res_huella)){
             $mensaje="8";
-            echo $repetido;
+            echo $res_huella;
         }
 
     }
@@ -25,10 +29,9 @@ if(isset($_POST['guardar_persona'])){
         echo "<script>
         alert('Un campo esta vacio');window.history.back()</script>";
     }
-
     
     if($mensaje=='8'){$mensaje="0";$huella="0";
-        echo $repetido;
+       
         //echo "<script>
         //alert('El usuario ya esta registrado');window.history.back()</script>";
     }
