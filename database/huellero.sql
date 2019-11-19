5c20e65409e39d6724da65fbd850a0a2535b2fdc
JesusWiedii <jesus.becerra@wiedii.co>
2019-11-19 16:49:02 -0500

    falto la base de datos XD

--- database/huellero.sql
+++ database/huellero.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost:8889
--- Tiempo de generación: 15-11-2019 a las 21:24:37
+-- Tiempo de generación: 19-11-2019 a las 21:48:10
 -- Versión del servidor: 5.7.26
 -- Versión de PHP: 7.3.8
 
@@ -38,13 +38,27 @@ CREATE TABLE `equipos` (
 --
 
 INSERT INTO `equipos` (`Id_equipo`, `nombre_equipo`, `hora_entrada`, `hora_salida`) VALUES
-(1, 'Colocolo', '13:00:00', '17:00:00'),
-(2, 'Lions', '09:00:00', '17:00:00'),
-(3, 'Margay', '07:00:00', '17:00:00'),
-(4, 'Geofrray', '07:00:00', '17:00:00'),
-(5, 'Cheetah', '07:00:00', '17:00:00'),
-(6, 'Ligers', '07:00:00', '17:00:00'),
-(7, 'Puma', '07:00:00', '17:00:00');
+(1, 'Colocolo', '00:00:00', '17:00:00'),
+(2, 'Lions', '00:00:00', '17:00:00'),
+(3, 'Margay', '00:00:00', '17:00:00'),
+(4, 'Geofrray', '00:00:00', '17:00:00'),
+(5, 'Cheetah', '00:00:00', '17:00:00'),
+(6, 'Ligers', '00:00:00', '17:00:00'),
+(7, 'Puma', '00:00:00', '17:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `registro`
+--
+
+CREATE TABLE `registro` (
+  `Id_fecha` int(11) NOT NULL,
+  `Fecha` date NOT NULL,
+  `entry_time` time NOT NULL,
+  `departure_time` time NOT NULL,
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -56,23 +70,22 @@ CREATE TABLE `usuarios` (
   `id` int(11) NOT NULL,
   `nombre_usu` varchar(255) DEFAULT NULL,
   `huella` varchar(255) NOT NULL,
-  `contraseña` varchar(255) NOT NULL,
+  `contrasena` varchar(255) NOT NULL,
   `Id_equipo` int(11) NOT NULL,
   `correo` varchar(255) NOT NULL,
   `fecha` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `estado` char(2) NOT NULL,
-  `admin` char(2) NOT NULL
+  `admini` char(2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `usuarios`
 --
 
-INSERT INTO `usuarios` (`id`, `nombre_usu`, `huella`, `contraseña`, `Id_equipo`, `correo`, `fecha`, `estado`, `admin`) VALUES
-(113, 'dscadsVZCV', '3423', '', 2, 'holi', '2019-11-14 14:17:45', 'Si', ''),
-(115, 'fadf', '31243576', '', 1, 'dsafa', '2019-11-14 20:06:19', 'Si', ''),
-(116, 'dafd5', '1234', '', 4, 'fqwerwe', '2019-11-14 20:24:51', 'Si', ''),
-(117, 'dscadsVZCV', '123', '', 1, 'dsafa', '2019-11-14 20:44:05', 'Si', '');
+INSERT INTO `usuarios` (`id`, `nombre_usu`, `huella`, `contrasena`, `Id_equipo`, `correo`, `fecha`, `estado`, `admini`) VALUES
+(121, 'Jesus Becerra ', '1234', '1234', 1, 'becerra.jesusantonio@gmail.com', '2019-11-19 19:55:33', 'Si', 'No'),
+(138, 'Jesus Becerra R', '12345', '12345', 1, 'jesus.becerra@wiedii.co', '2019-11-19 20:46:09', 'Si', 'No'),
+(139, 'yorluis', '123', '123', 1, 'yorluis.vega@wiedii.co', '2019-11-19 20:50:15', 'Si', 'No');
 
 --
 -- Índices para tablas volcadas
@@ -84,6 +97,13 @@ INSERT INTO `usuarios` (`id`, `nombre_usu`, `huella`, `contraseña`, `Id_equipo`
 ALTER TABLE `equipos`
   ADD PRIMARY KEY (`Id_equipo`),
   ADD UNIQUE KEY `nombre_equipo` (`nombre_equipo`);
+
+--
+-- Indices de la tabla `registro`
+--
+ALTER TABLE `registro`
+  ADD PRIMARY KEY (`Id_fecha`),
+  ADD KEY `id` (`id`);
 
 --
 -- Indices de la tabla `usuarios`
@@ -104,14 +124,26 @@ ALTER TABLE `equipos`
   MODIFY `Id_equipo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
+-- AUTO_INCREMENT de la tabla `registro`
+--
+ALTER TABLE `registro`
+  MODIFY `Id_fecha` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=118;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=140;
 
 --
 -- Restricciones para tablas volcadas
 --
+
+--
+-- Filtros para la tabla `registro`
+--
+ALTER TABLE `registro`
+  ADD CONSTRAINT `registro_ibfk_1` FOREIGN KEY (`id`) REFERENCES `usuarios` (`id`);
 
 --
 -- Filtros para la tabla `usuarios`
